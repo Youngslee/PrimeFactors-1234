1815d7b6f409305755600a467cf51a8c0be8562c
youngslee <popopo3204@naver.com>
2023-07-05 15:14:42 +0900

    [feature] 33

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,8 +1,9 @@
 #include "pch.h"
-
+#include <vector>
 #include "../Project13/prime-factors.cpp"
 using namespace std;
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(TestCaseName, Of1) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {};
+	EXPECT_EQ(expected, prime_factor.of(1));
 }
